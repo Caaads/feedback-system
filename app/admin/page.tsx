9e14e677e7972a692d18206597cfa39ffc8fa2fd
Caaads <alfred.mari2004@gmail.com>
2026-01-15 01:34:21 +0800

    second commit

---
+++ app/admin/page.tsx
@@ -0,0 +1,12 @@
+"use client"
+
+import { useEffect, useState } from "react"
+
+export default function AdminPage() {
+  return (
+    <div>
+      <h1>Admin Dashboard</h1>
+            <p>Feedback analytics will appear here.</p>
+    </div>
+  )
+}
